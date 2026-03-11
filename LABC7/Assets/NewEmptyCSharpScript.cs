1c51cf479ad0f5f0b443f2942a0e495761244d44
nguyen1405 <148754154+nguyen1405@users.noreply.github.com>
2026-03-11 09:05:41 +0700

    labc7

---
+++ LABC7/Assets/NewEmptyCSharpScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class DebugLog : MonoBehaviour
+{
+    public void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    public void Option()
+    {
+        Debug.Log("Option");
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit");
+    }
+}
